56997abf911271ac8243e8b10a0d39020dd7ce28
Liangjun Jiang <liangjun.jiang@gmail.com>
2013-02-08 11:23:33 -0600

    add test ui

---
+++ TestView/TestModeViewController.h
@@ -0,0 +1,13 @@
+//
+//  TestModeViewController.h
+//  SAT
+//
+//  Created by LIANGJUN JIANG on 2/8/13.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TestModeViewController : UITableViewController
+
+@end
---
+++ TestView/TestTableViewController.m
@@ -0,0 +1,166 @@
+/*
+     File: MyTableViewController.m
+ Abstract: The main view controller of this app.
+  Version: 1.3
+ 
+ Disclaimer: IMPORTANT:  This Apple software is supplied to you by Apple
+ Inc. ("Apple") in consideration of your agreement to the following
+ terms, and your use, installation, modification or redistribution of
+ this Apple software constitutes acceptance of these terms.  If you do
+ not agree with these terms, please do not use, install, modify or
+ redistribute this Apple software.
+ 
+ In consideration of your agreement to abide by the following terms, and
+ subject to these terms, Apple grants you a personal, non-exclusive
+ license, under Apple's copyrights in this original Apple software (the
+ "Apple Software"), to use, reproduce, modify and redistribute the Apple
+ Software, with or without modifications, in source and/or binary forms;
+ provided that if you redistribute the Apple Software in its entirety and
+ without modifications, you must retain this notice and the following
+ text and disclaimers in all such redistributions of the Apple Software.
+ Neither the name, trademarks, service marks or logos of Apple Inc. may
+ be used to endorse or promote products derived from the Apple Software
+ without specific prior written permission from Apple.  Except as
+ expressly stated in this notice, no other rights or licenses, express or
+ implied, are granted by Apple herein, including but not limited to any
+ patent rights that may be infringed by your derivative works or by other
+ works in which the Apple Software may be incorporated.
+ 
+ The Apple Software is provided by Apple on an "AS IS" basis.  APPLE
+ MAKES NO WARRANTIES, EXPRESS OR IMPLIED, INCLUDING WITHOUT LIMITATION
+ THE IMPLIED WARRANTIES OF NON-INFRINGEMENT, MERCHANTABILITY AND FITNESS
+ FOR A PARTICULAR PURPOSE, REGARDING THE APPLE SOFTWARE OR ITS USE AND
+ OPERATION ALONE OR IN COMBINATION WITH YOUR PRODUCTS.
+ 
+ IN NO EVENT SHALL APPLE BE LIABLE FOR ANY SPECIAL, INDIRECT, INCIDENTAL
+ OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ INTERRUPTION) ARISING IN ANY WAY OUT OF THE USE, REPRODUCTION,
+ MODIFICATION AND/OR DISTRIBUTION OF THE APPLE SOFTWARE, HOWEVER CAUSED
+ AND WHETHER UNDER THEORY OF CONTRACT, TORT (INCLUDING NEGLIGENCE),
+ STRICT LIABILITY OR OTHERWISE, EVEN IF APPLE HAS BEEN ADVISED OF THE
+ POSSIBILITY OF SUCH DAMAGE.
+ 
+ Copyright (C) 2010 Apple Inc. All Rights Reserved.
+ 
+ */
+
+#import "TestTableViewController.h"
+
+@implementation TestTableViewController
+
+@synthesize myHeaderView, myFooterView, tableArray;
+
+-(id)initWithCoder:(NSCoder *)aDecoder
+{
+    self = [super initWithNibName:@"TestTableViewController" bundle:nil];
+    if (self) {
+        
+    }
+    
+    return self;
+}
+
+
+- (void)viewDidLoad
+{
+	// setup our table data
+	self.tableArray = @[@"Camping", @"Water Skiing", @"Weight Lifting", @"Stamp Collecting"];
+	
+	// set up the table's header view based on our UIView 'myHeaderView' outlet
+	CGRect newFrame = CGRectMake(0.0, 0.0, self.tableView.bounds.size.width, self.myHeaderView.frame.size.height);
+	self.myHeaderView.backgroundColor = [UIColor clearColor];
+	self.myHeaderView.frame = newFrame;
+	self.tableView.tableHeaderView = self.myHeaderView;	// note this will override UITableView's 'sectionHeaderHeight' property
+	
+	// set up the table's footer view based on our UIView 'myFooterView' outlet
+	newFrame = CGRectMake(0.0, 0.0, self.tableView.bounds.size.width, self.myFooterView.frame.size.height);
+	self.myFooterView.backgroundColor = [UIColor clearColor];
+	self.myFooterView.frame = newFrame;
+	self.tableView.tableFooterView = self.myFooterView;	// note this will override UITableView's 'sectionFooterHeight' property
+}
+
+// called after the view controller's view is released and set to nil.
+// For example, a memory warning which causes the view to be purged. Not invoked as a result of -dealloc.
+// So release any properties that are loaded in viewDidLoad or can be recreated lazily.
+//
+- (void)viewDidUnload
+{
+	self.myHeaderView = nil;
+	self.myFooterView = nil;
+	self.tableArray = nil;
+}
+
+
+
+#pragma mark -
+#pragma mark UITableViewDataSource
+
+- (NSString *)tableView:(UITableView *)tableView titleForHeaderInSection:(NSInteger)section
+{
+	return @"Hobby Information:";
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+	return [tableArray count];
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+	static NSString *kCellID = @"cellID";
+	
+	UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:kCellID];
+	if (cell == nil)
+	{
+		cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:kCellID];
+		cell.accessoryType = UITableViewCellAccessoryNone;
+		cell.selectionStyle = UITableViewCellSelectionStyleNone;
+	}
+	
+	cell.textLabel.text = tableArray[[indexPath row]];
+	
+	return cell;
+}
+
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    
+    // if there is a selected cell, we need to change its background to the default
+    // This works but I am expecting a better way
+    NSArray *cells = [tableView visibleCells];
+    [cells enumerateObjectsUsingBlock:^(id obj, NSUInteger idx, BOOL *stop) {
+        UITableViewCell *cell = (UITableViewCell *)obj;
+        if (![cell.backgroundColor isEqual:[UIColor whiteColor]]) {
+            cell.backgroundColor = [UIColor whiteColor];
+        }
+    }];
+    
+    // then we will change the tableview cell background color for a correct guess or a false guess
+    NSUInteger row = indexPath.row;
+    
+    UITableViewCell *selectedCell = [tableView cellForRowAtIndexPath:indexPath];
+    
+    if (row == 2) {
+        selectedCell.backgroundColor = [UIColor greenColor];
+    } else
+        selectedCell.backgroundColor = [UIColor redColor];
+    
+}
+
+#pragma mark -
+#pragma mark Action methods
+
+- (IBAction)button1Action:(id)sender
+{
+	// Button1 was pressed
+}
+
+- (IBAction)button2Action:(id)sender
+{
+	// Button2 was pressed
+}
+
+@end
+
